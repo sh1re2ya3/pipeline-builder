0bf26bd2336c9884ada706d40c6eabfacb193e0e
sh1re2ya3 <singalshreya97@gmail.com>
2026-03-11 22:38:31 +0530

    api call correction

--- frontend/src/bottomBar.js
+++ frontend/src/bottomBar.js
@@ -29,7 +29,7 @@ export const BottomBar = () => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/pipelines/parse', {
+      const response = await fetch('/api/pipelines/parse', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
